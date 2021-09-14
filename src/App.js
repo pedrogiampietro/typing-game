18e58c97db1823bc91593de448892c1ccae65858
Pedro Giampietro <pedro.giampietro@l8group.net>
2021-09-14 15:30:24 -0300

    added a css and struture index.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <h1> Hello World </h1>;
+export function App() {
+  return (
+    <div className="container">
+      <div className="valid-keys">
+        <span className="matched">Ped</span>
+        <span className="remainder">ro</span>
+      </div>
+      <div className="typed-keys">asdfpedtkoa</div>
+      <div className="completed-words">
+        <ol>
+          <li>cidade</li>
+          <li>carro</li>
+          <li>profissional</li>
+        </ol>
+      </div>
+    </div>
+  );
 }
-
-export default App;
